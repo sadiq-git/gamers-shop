0bfdc33b03063298bd73718508b3db28c7679f08
sadiq Karur <33385932+sadiqmuhamed@users.noreply.github.com>
2019-05-02 17:56:12 +0530

    Delete firebase-key.ts

--- src/private/firebase-key.ts
+++
@@ -1,8 +0,0 @@
-export const fireBaseKey = {
-    apiKey: 'AIzaSyBwtVcQZOyp0eQC4DzdTrgRT6NJaqbBBkY',
-    authDomain: 'oshop-ritwick.firebaseapp.com',
-    databaseURL: 'https://oshop-ritwick.firebaseio.com',
-    projectId: 'oshop-ritwick',
-    storageBucket: '',
-    messagingSenderId: '350844826380'
-};
